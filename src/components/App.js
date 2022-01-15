14709b4190d5f3fd27b1755a18eaece61c1c7b81
ClaraVilelaJato <claravilelajato@gmail.com>
2022-01-15 11:08:55 +0100

    Title html structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
-
-import '../styles/App.css';
+import "../styles/App.css";
 
 function App() {
   return (
     <div className="App">
-     <h1>Hola mundo</h1>
+      <header className="container">
+        <section className="left">
+          <div className="title">
+            <h1>
+              Planning/Nombre del Proyecto
+              <button className="btn-title">
+                <i className="fas fa-chevron-down"></i>
+              </button>
+              <button className="btn-title">
+                <i className="fas fa-star"></i>
+              </button>
+            </h1>
+          </div>
+        </section>
+      </header>
     </div>
   );
 }
